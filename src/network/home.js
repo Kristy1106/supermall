5634d0fc1feca6a581f2a9402535306f297afea3
kristy <478364027@qq.com>
2022-06-22 14:56:45 +0800

    20220622

--- src/network/home.js
+++ src/network/home.js
@@ -6,4 +6,24 @@ export function getHomeMultidata() {
   })
 }
 
+export function getHomeGoods(type,page) {
+  return request({
+    url: '/home/data',
+    params: {
+      type,
+      page
+    }
+  })
+}
 
+// 把一个数组放到另一个数组里
+let totalNums = []
+const nums1 = [20,11,222]
+const nums2 = [111,222,333]
+
+// for(let n of nums1) {
+//   totalNums.push(n)
+// }
+
+//解构，依次塞到totalNums里面,之后num2再放数据的时候前数据依旧存在
+totalNums.push(...nums1)
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,43 +1,99 @@
 <template>
   <div id="Home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
+    <home-swiper :banners="banners"/>
     <feature-view/>
-
+    <tab-control class="tab-control"
+                 :title="['流行','新款','精选']"
+                 @tabClick="tabClick"/>
+    <goods-list :goods="goods[currentType].list"/>
   </div>
 </template>
 
 <script>
 
-import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper";
 import RecommendView from "./childComps/RecommendView";
 import FeatureView from "./childComps/FeatureView";
 
-import {getHomeMultidata} from "network/home";
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
+
+import {
+  getHomeMultidata,
+  getHomeGoods
+} from "network/home";
+import GoodsList from "@/components/content/goods/GoodsList";
 
 export default {
   name: "Home",
   components: {
-    NavBar,
+    GoodsList,
     HomeSwiper,
     RecommendView,
-    FeatureView
+    FeatureView,
+    NavBar,
+    TabControl
   },
   //data用于存储返回的数据
   data() {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      /*请求数据，往list数组里放*/
+      goods: {
+        'pop': {page: 0,list: []},
+        'news': {page: 0,list: []},
+        'sell': {page: 0,list: []}
+      },
+      currentType: 'pop'
     }
   },
   created() {
     //1.请求多个数据
-    getHomeMultidata().then(res => {
-      //console.log(res);
-      this.banners =res.banner.list;
-      this.recommends = res.recommend.list;
-    })
+    this.getHomeMultidata()
+
+    //2.请求商品数据
+    this.getHomeGoods('pop')
+    // this.getHomeGoods('new')
+    // this.getHomeGoods('sell')
+  },
+  methods: {
+    /*
+    * 事件监听相关方法
+    * */
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = 'pop'
+          break
+        case 1:
+          this.currentType = 'new'
+          break
+        case 2:
+          this.currentType = 'sell'
+          break
+      }
+    },
+    /*
+    * 网络请求相关方法
+    * */
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        //console.log(res);
+        this.banners =res.banner.list;
+        this.recommends = res.recommend.list;
+      })
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then(res => {
+        //console.log(res);
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+      })
+    }
   }
 }
 </script>
@@ -56,5 +112,10 @@ export default {
     right: 0;
     top: 0;
     z-index: 9;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
